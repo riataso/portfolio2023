fd58ee2485968ae21824c423d39d7f9d643eb374
riataso <nkc20212452@denpa.jp>
2022-04-18 12:55:33 +0900

    コード修正

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
         <div class="row">
           <div class="col-lg-7 mx-auto text-center">
             <h1 class="display-4 text-white ">Story</h1>
-            <p class="text-white my-3">
-            工業高校で資格取得に力を入れながら自主的にITを学び工業高校卒業後、ITの専門学校に進学現在は2年目になり専門知識を身に着けながら日々自主開発としてWebの勉強・開発をしています
+            <p class="text-white my-6">
+            工業高校で資格取得に力を入れながら自主的にITを学び工業高校卒業後、ITの専門学校に進学,現在は2年目になり専門知識を身に着けながら日々自主開発としてWebの勉強・開発をしています
             </p>
           </div>
         </div>
